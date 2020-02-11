8988b2e7b46a00a4e4c74e9ef9515168c813bd6e
Alizee Jan <jan.alizee@gmail.com>
2020-02-11 12:09:04 +0100

    iteration 2 error

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import contacts from './contacts.json';
+
 class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5)
+  }
+
+  addRandomContact = () => {
+    const contactsCopy = [...this.state.contacts];
+    const randomContact = contacts[Math.floor(Math.random()*contacts.lenght)];
+    contactsCopy.push(randomContact);
+    
+    this.setState({
+      contacts: contactsCopy
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <button onClick={(event) => this.addRandomContact()}>Add Random Contact</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.contacts.map((item, index) => (
+            <tr>
+              <td key={index}><img src={item.pictureUrl} alt=""/></td>
+              <td>{item.name}</td>
+              <td>{item.popularity.toFixed(2)}</td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
       </div>
     );
   }
